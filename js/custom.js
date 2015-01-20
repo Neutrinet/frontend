ccbf70d8066270419001190b74f230b33b3214d1
Wannes <wannes321@gmail.com>
2015-01-20 22:33:22 +0100

    This commit is not worth looking at. Seriously. This commit is certainly not part of a secret plan to test and ultimately modify our logo.

--- js/custom.js
+++ js/custom.js
@@ -20,6 +20,42 @@
         }
     });
 
+
+    //jQuery to inline SVG so it's stylable which makes me so happy
+    /*
+     * http://stackoverflow.com/questions/11978995/how-to-change-color-of-svg-image-using-css-jquery-svg-image-replacement
+     * Replace all SVG images with inline SVG
+     */
+    jQuery('img.svg').each(function(){
+    	var $img = jQuery(this);
+        var imgID = $img.attr('id');
+        var imgClass = $img.attr('class');
+        var imgURL = $img.attr('src');
+
+        jQuery.get(imgURL, function(data) {
+            // Get the SVG tag, ignore the rest
+            var $svg = jQuery(data).find('svg');
+
+            // Add replaced image's ID to the new SVG
+            if(typeof imgID !== 'undefined') {
+                $svg = $svg.attr('id', imgID);
+            }
+            // Add replaced image's classes to the new SVG
+            if(typeof imgClass !== 'undefined') {
+                $svg = $svg.attr('class', imgClass+' replaced-svg');
+            }
+
+            // Remove any invalid XML tags as per http://validator.w3.org
+            $svg = $svg.removeAttr('xmlns:a');
+
+            // Replace image with new SVG
+            $img.replaceWith($svg);
+
+        }, 'xml');
+
+    });
+	
+
     function addMeetings(url, kind) {
         var element = $("#" + kind + "-meetings");
         $.getJSON(url).success(function(data){
